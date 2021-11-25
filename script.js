d67fbea445e7c6982da6a3d3e53ce7728955e081
james4-daf <d.james@elsevier.com>
2021-11-25 11:04:05 +0000

    1.02

---
+++ script.js
@@ -0,0 +1,7 @@
+const getTipAmount = () => {
+  let billAmount = document.getElementById("theInput").billAmount;
+  let tipAmount = document.getElementById("theInput").tipAmount;
+  //let value = document.getElementById("theInput").value;
+  let totalAmount = billAmount * tipAmount;
+  document.getElementById("billOutput").totalAmount;
+};
